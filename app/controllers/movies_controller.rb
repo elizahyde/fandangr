aaf9249a64f9fc8615265cb8d6f409e23e8b140c
elizahyde <elizabeth@elizahyde.com>
2013-07-07 21:55:18 -0700

    readding those controllers

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,24 @@
+class MoviesController < ApplicationController
+  def new
+    @movie = Movie.new
+  end
+
+  def index
+    @movies = Movie.all
+    @tickets = Ticket.all
+
+  end
+
+  def create
+    @movie = Movie.new(params[:movie])
+    if @movie.save
+      redirect_to movie_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+end
